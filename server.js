650361668da22624fb36a154858c72aafd7647b4
tahsun1462 <tafa4205@gmail.com>
2025-02-04 22:59:41 +0600

    commit

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const { exec } = require("child_process");
 const app = express();
 const git = simpleGit();
 
-// Set Git global configuration for username and email
 git.raw(['config', '--global', 'user.name', 'tahsun1462']);
 git.raw(['config', '--global', 'user.email', 'tafa4205@gmail.com']);
 
 require('dotenv').config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const REPO_URL = `https://${GITHUB_TOKEN}@github.com/TaHsUN1462/SmartKhata.git`;  // Use your repo URL
+const REPO_URL = `https://${GITHUB_TOKEN}@github.com/TaHsUN1462/SmartKhata.git`;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -42,16 +41,13 @@ app.post("/data", async (req, res) => {
     } else {
       console.log("Data Updated Successfully.");
 
-      // Manually set the remote repository
-      exec(`git remote set-url origin https://${GITHUB_TOKEN}@github.com/TaHsUN1462/SmartKhata.git`, (error, stdout, stderr) =>
-      {
+      exec(`git remote set-url origin https://${GITHUB_TOKEN}@github.com/TaHsUN1462/SmartKhata.git`, (error, stdout, stderr) => {
         if (error) {
           console.error("Git Remote Set Error:", stderr);
           return res.status(500).json({ error: "Git Remote Set Failed" });
         }
         console.log("Git Remote Set Successfully:", stdout);
 
-        // Pull the latest changes from GitHub
         exec("git pull origin main", (error, stdout, stderr) => {
           if (error) {
             console.error("Git Pull Error:", stderr);
@@ -59,12 +55,11 @@ app.post("/data", async (req, res) => {
           }
           console.log("Git Pulled Successfully:", stdout);
 
-          // Add, commit, and push the updated data.json file to GitHub
           git.add("./data.json")
             .then(() => git.commit("Updated data.json from API"))
             .then(() => git.push(REPO_URL, "main"))
             .then(() => {
-              git.log().then(log => console.log("Git Log:", log));  // Add log for debugging
+              console.log("Git Push Success!");
               res.json({ message: "Updated & pushed successfully" });
             })
             .catch(gitErr => {
